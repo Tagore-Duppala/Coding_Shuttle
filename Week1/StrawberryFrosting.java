0a974dd66373bb3dd6c2dbb885013972b82676cd
Tagore-Duppala <60137555+Tagore-Duppala@users.noreply.github.com>
2024-08-16 18:19:53 +0530

    Update StrawberryFrosting.java

--- Week1/StrawberryFrosting.java
+++ Week1/StrawberryFrosting.java
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConditionalOnProperty(name = "Flavor2", havingValue = "StrawberryFrost")
+@ConditionalOnProperty(name = "Flavor2", havingValue = "StrawberryFrost")   // It is same implementation as shown in ChocolateFrosting, Please refer that for more comments
 public class StrawberryFrosting implements Frosting {
 
     @Override
