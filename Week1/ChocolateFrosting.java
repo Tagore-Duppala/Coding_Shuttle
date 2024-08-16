f597f62d477736610574fd999cfbc6fdabe3b666
Tagore-Duppala <60137555+Tagore-Duppala@users.noreply.github.com>
2024-08-16 18:18:55 +0530

    Update ChocolateFrosting.java

--- Week1/ChocolateFrosting.java
+++ Week1/ChocolateFrosting.java
@@ -6,7 +6,10 @@ import org.springframework.stereotype.Component;
 
 @Component
 @ConditionalOnProperty(name = "Flavor1", havingValue = "ChocolateFrost")
-//@Primary
+//@Primary                                 // We need to declare primary annotation if there is no condition and if single interface is implemented by multiple classes
+                                           // When we inject a bean into some class and if the implemented method from interface was called then it creates confusion and throws an error stating method present in multiple beans
+                                           // To avoid this we mark one class as primary, If we wanted to make it dynamic then, We put the ConditiononProperty and we set the condtions in app.configuration
+                                            // Note there are several other annotations as well ->  ClassOnPrperty etc
 public class ChocolateFrosting implements Frosting {
 
     @Override
