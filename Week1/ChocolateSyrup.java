ad50d6073a649e96d792f597bb13ef5215b9392e
Tagore-Duppala <60137555+Tagore-Duppala@users.noreply.github.com>
2024-08-16 18:06:39 +0530

    Update ChocolateSyrup.java

--- Week1/ChocolateSyrup.java
+++ Week1/ChocolateSyrup.java
@@ -3,7 +3,7 @@ package com.codingshuttle.week1.Introductiontospring;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
-@Component // Defining it as a Bean in Springboot
+@Component                                                               // Defining it as a Bean in Springboot
 @ConditionalOnProperty(name = "Flavor1", havingValue = "ChocolateSyrup") // Setting a condition which executes the below class when satisfied.
                                                                         // Conditions can be set manually in app configiration
 public class ChocolateSyrup implements Syrup {
