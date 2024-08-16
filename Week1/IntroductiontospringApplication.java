33d9197b5cb8e28e4de457841e467dfe4145b51c
Tagore-Duppala <60137555+Tagore-Duppala@users.noreply.github.com>
2024-08-16 18:29:41 +0530

    Update IntroductiontospringApplication.java

--- Week1/IntroductiontospringApplication.java
+++ Week1/IntroductiontospringApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class IntroductiontospringApplication implements CommandLineRunner {
+@SpringBootApplication							// This is the starting point of a springboot application, Only one annotation of this type should be present for an application.
+public class IntroductiontospringApplication implements CommandLineRunner { 
+									// When we implement CommandLineRunner SpringBoot automatically calls run() method
+									// Order of Execution in Main method -> run method in main (Starts SpringBoot application) -> run method()
 
 	@Autowired
 	CakeBaker Cake;
