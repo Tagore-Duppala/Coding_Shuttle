3394eba50c917f1dc727bbf7402dc77adb9aefbf
Tagore-Duppala <60137555+Tagore-Duppala@users.noreply.github.com>
2024-08-16 18:10:24 +0530

    Update Syrup.java

--- Week1/Syrup.java
+++ Week1/Syrup.java
@@ -1,5 +1,6 @@
 package com.codingshuttle.week1.Introductiontospring;
 
-public interface Syrup {
+public interface Syrup {               // Creating an interface which is implemented by other classes
+                                     // Note that we need not declare an Interface as Bean, If the implemented class is declared as a Bean
     void getSyrupType();
 }
