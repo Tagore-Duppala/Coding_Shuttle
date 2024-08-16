bba8dba8ad4188a264bcbe6cb74425c124205e78
Tagore-Duppala <60137555+Tagore-Duppala@users.noreply.github.com>
2024-08-16 18:25:38 +0530

    Update CakeBaker.java

--- Week1/CakeBaker.java
+++ Week1/CakeBaker.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 @Component                        //Used to define as Java Bean, There are other annotations like @Service, @Repository, @Controller to define a class as a Bean, but Spring categorize them into different groups, But inturn everything is a Bean.
 public class CakeBaker {
 
-    @Autowired
+    @Autowired                    // Used for Dependency Injection, It injects the dependents, Here it is Frosting Interface which was implemented by multiple classes and those classes were declared as beans, So need not declare interface as a bean.
     Frosting Frost;
 
     @Autowired
